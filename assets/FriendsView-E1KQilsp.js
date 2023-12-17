6d88f826c7df9ba6f73cc320cd0a1be8bb4ec515
MrRyhor <grigory.karpyak@gmail.com>
2023-12-17 19:56:38 +0100

    Updates

---
+++ assets/FriendsView-E1KQilsp.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,M as r}from"./index-fGX4PBKn.js";const n={__name:"FriendsView",setup(s){return(t,c)=>(e(),a(r))}};export{n as default};
---
+++ assets/GiftsView-4U5q6vXw.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,M as t}from"./index-fGX4PBKn.js";const _={__name:"GiftsView",setup(r){return(s,c)=>(e(),a(t))}};export{_ as default};
